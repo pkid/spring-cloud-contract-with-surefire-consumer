47a6f64631bec1b19b323c8ff9f5a7b3da59a3ca
Patrick Ghezali <patrick.ghezali@sap.com>
2017-03-13 13:14:06 +0100

    Update help_new.groovy

--- help_new.groovy
+++ help_new.groovy
@@ -26,3 +26,43 @@ def tagChangesToGit(version) {
     """
 }
 
+def uploadArtifactsToNexus(nexus_url, nexus_repo){
+    sh "mvn -DperformRelease=true deploy -DaltDeploymentRepository=$nexus_repo::default::$nexus_url$nexus_repo"
+}
+
+/*
+ * This function builds a docker image using the Dockerfile and push the image to Artifactory
+ */
+def buildDockerImageAndPushToArtifactory(url, repo, user, password){
+    if(! fileExists ('Dockerfile')){
+        echo 'Dockerfile does not exist - skip building docker images'
+        return
+    }
+    
+    def pomVersion = executeShell 'mvn -q -Dexec.executable=\'echo\' -Dexec.args=\'${project.version}\' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec'
+    def artifactId = executeShell 'mvn -q -Dexec.executable=\'echo\' -Dexec.args=\'${project.artifactId}\' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec'
+    
+    def imageName = url + repo + ":" + pomVersion
+    def jarName = artifactId + "-" + pomVersion + ".jar"
+
+    sh "docker login -u $user -p $password $url"
+    sh "docker build --build-arg JARNAME=$jarName -t  $imageName ."
+    sh "docker push $imageName"
+    sh "docker rmi $imageName"
+    
+    return imageName
+}
+
+def getGithubInfo() {
+    def githubInfo = [:]
+    def tokens = "${env.JOB_NAME}".tokenize('/')
+    githubInfo['org'] = tokens[tokens.size()-3]
+    echo githubInfo['org']
+    githubInfo['repo'] = tokens[tokens.size()-2]
+    echo githubInfo['repo']
+    githubInfo['branch'] = tokens[tokens.size()-1]
+    echo githubInfo['branch']
+    return githubInfo
+}
+
+
