c38c8f129f87c92dc582ceb4cabc08e6fff84e80
Floriment Klinaku <floriment.klinaku@sap.com>
2017-04-20 10:47:12 +0000

    Instrumentation via Prometheus Client library

--- service/src/main/java/hello/Application.java
+++ service/src/main/java/hello/Application.java
@@ -3,6 +3,11 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
+
+@EnablePrometheusEndpoint
+@EnableSpringBootMetricsCollector
 @SpringBootApplication
 public class Application {
 
--- service/src/main/java/hello/GreetingController.java
+++ service/src/main/java/hello/GreetingController.java
@@ -5,14 +5,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.prometheus.client.Counter;
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+
 @RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    static final Counter requests = Counter.build()
+            .name("greeting_requests_total").help("Total requests.").register();
+         
+    
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        requests.inc();
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
