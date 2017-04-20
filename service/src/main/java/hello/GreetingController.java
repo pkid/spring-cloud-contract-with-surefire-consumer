c77f17ed891fb6f36c17ef9623985b7a06b67807
Yashu Liang <yashu.liang@sap.com>
2017-04-20 12:53:12 +0000

    add logging

--- service/src/main/java/hello/GreetingController.java
+++ service/src/main/java/hello/GreetingController.java
@@ -4,6 +4,8 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.prometheus.client.Counter;
 import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
@@ -16,11 +18,14 @@ public class GreetingController {
 
     static final Counter requests = Counter.build()
             .name("greeting_requests_total").help("Total requests.").register();
+    
+    private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
          
     
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         requests.inc();
+        logger.info("Greetings will be sent out");
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
