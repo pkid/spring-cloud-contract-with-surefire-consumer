02c1feb6789589f6116d785ee1abf08d69623ed0
Yashu Liang <yashu.liang@sap.com>
2017-05-08 09:47:07 +0000

    new

---
+++ service/src/test/resources/contracts/shouldBlah.groovy
@@ -0,0 +1,22 @@
+org.springframework.cloud.contract.spec.Contract.make {
+    request { // (1)
+        method 'GET' // (2)
+        url '/greeting' // (3)
+        //body([ // (4)
+        //       "client.id": $(regex('[0-9]{10}')),
+        //       loanAmount: 99999
+        //])
+        headers { // (5)
+            contentType('application/json')
+        }
+    }
+    response { // (6)
+        status 200 // (7)
+        body([ // (8)
+               "content": "Hello, Worl!"
+        ])
+        headers { // (9)
+            contentType('application/json;charset=UTF-8')
+        }
+    }
+}
